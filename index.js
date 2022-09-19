01dba2c63833dc45aede7e07cec55b91ca9d2d78
OksanaOlefirenko <oksana_olefirenko@ukr.net>
2022-09-19 16:35:45 +0300

    update invokeAction
    add start

--- index.js
+++ index.js
@@ -46,4 +46,12 @@ const invokeAction = async ({ action, id, name, email, phone }) => {
   }
 };
 
-invokeAction(argv);
+const start = async argv => {
+  try {
+    await invokeAction(argv);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start(argv);
